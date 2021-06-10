66988ea34c91ab823f3caf979aa5c88bfb5b7e9f
Anych <anuar123@mail.ru>
2021-06-11 03:35:24 +0600

    Wrote some pytest tests

--- my_pytest/companies/tests/test_unittest_api.py
+++ my_pytest/companies/tests/test_unittest_api.py
@@ -1,5 +1,6 @@
 import json
 from unittest import TestCase
+import logging
 
 import pytest
 from django.test import Client
@@ -84,3 +85,34 @@ class TestPostCompanies(BasicCompanyAPITestCase):
     @pytest.mark.skip
     def test_should_be_skipped(self) -> None:
         self.assertEqual(1, 2)
+
+
+def raise_covid19_exception() -> None:
+    raise ValueError('CoronaVirus Exception')
+
+
+def test_raise_covid19_exception_should_pass() -> None:
+    with pytest.raises(ValueError) as e:
+        raise_covid19_exception()
+    assert 'CoronaVirus Exception' == str(e.value)
+
+
+logger = logging.getLogger('CORONA_LOGS')
+
+
+def function_that_logs() -> None:
+    try:
+        raise ValueError('CoronaVirus Exception')
+    except ValueError as e:
+        logger.warning(f'I am logging {str(e)}')
+
+
+def test_logged_warning_level(caplog) -> None:
+    function_that_logs()
+    assert 'I am logging CoronaVirus Exception' in caplog.text
+
+
+def test_logged_info_level(caplog) -> None:
+    with caplog.at_level(logging.INFO):
+        logger.info('I am logging info level')
+        assert 'I am logging info level' in caplog.text
