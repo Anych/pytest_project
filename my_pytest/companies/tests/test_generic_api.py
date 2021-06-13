6ed90b3688212d0b5f035b0d9761f2cf5f99a82f
Anych <anuar123@mail.ru>
2021-06-14 02:33:29 +0600

    THE END

--- my_pytest/companies/tests/test_generic_api.py
+++ my_pytest/companies/tests/test_generic_api.py
@@ -8,6 +8,7 @@ testing_env_companies_url = "http://127.0.0.1:8000/companies/"
 
 
 @pytest.mark.skip_in_ci
+@pytest.mark.skip(reason='This test needs localhost django server')
 def test_zero_companies_django_agnostic() -> None:
     response = requests.get(url=testing_env_companies_url)
     assert response.status_code == 200
@@ -15,34 +16,35 @@ def test_zero_companies_django_agnostic() -> None:
 
 
 @pytest.mark.skip_in_ci
+@pytest.mark.skip(reason='This test needs localhost django server')
 def test_create_company_with_layoffs_django_agnostic() -> None:
     response = requests.post(
         url=testing_env_companies_url,
-        json={"name": "test company name", "status": "Layoffs"},
+        json={'name': 'test company name', 'status': 'Layoffs'},
     )
     assert response.status_code == 201
     response_content = json.loads(response.content)
-    assert response_content.get("status") == "Layoffs"
+    assert response_content.get('status') == 'Layoffs'
 
-    cleanup_company(company_id=response_content["id"])
+    cleanup_company(company_id=response_content['id'])
 
 
 def cleanup_company(company_id: str) -> None:
-    response = requests.delete(url=f"http://127.0.0.1:8000/companies/{company_id}")
+    response = requests.delete(url=f'http://127.0.0.1:8000/companies/{company_id}')
     assert response.status_code == 204
 
 
 @pytest.mark.crypto
 def test_dogecoin_api() -> None:
     response = requests.get(
-        url="https://api.cryptonator.com/api/ticker/doge-usd",
-        headers={"User-Agent": "Mozilla/5.0"},
+        url='https://api.cryptonator.com/api/ticker/doge-usd',
+        headers={'User-Agent': 'Mozilla/5.0'},
     )
 
     assert response.status_code == 200
     response_content = json.loads(response.content)
-    assert response_content["ticker"]["base"] == "DOGE"
-    assert response_content["ticker"]["target"] == "USD"
+    assert response_content['ticker']['base'] == 'DOGE'
+    assert response_content['ticker']['target'] == 'USD'
 
 
 @pytest.mark.crypto
@@ -50,18 +52,18 @@ def test_dogecoin_api() -> None:
 def test_mocked_dogecoin_api() -> None:
     responses.add(
         method=responses.GET,
-        url="https://api.cryptonator.com/api/ticker/doge-usd",
+        url='https://api.cryptonator.com/api/ticker/doge-usd',
         json={
-            "ticker": {
-                "base": "ANUAR",
-                "target": "ANUAR-USD",
-                "price": "0.04535907",
-                "volume": "4975940509.75870037",
-                "change": "-0.00052372",
+            'ticker': {
+                'base': 'ANUAR',
+                'target': 'ANUAR-USD',
+                'price': '0.04535907',
+                'volume': '4975940509.75870037',
+                'change': '-0.00052372',
             },
-            "timestamp": 1612515303,
-            "success": True,
-            "error": "",
+            'timestamp': 1612515303,
+            'success': True,
+            'error': '',
         },
         status=200,
     )
@@ -71,16 +73,16 @@ def test_mocked_dogecoin_api() -> None:
 
 def process_crypto() -> int:
     response = requests.get(
-        url="https://api.cryptonator.com/api/ticker/doge-usd",
-        headers={"User-Agent": "Mozilla/5.0"},
+        url='https://api.cryptonator.com/api/ticker/doge-usd',
+        headers={'User-Agent': 'Mozilla/5.0'},
     )
 
     response_content = json.loads(response.content)
     if response.status_code != 200:
-        raise ValueError("Request to Crypto API FAILED!")
+        raise ValueError('Request to Crypto API FAILED!')
 
-    coin_name = response_content["ticker"]["base"]
-    if coin_name == "ANUAR":
+    coin_name = response_content['ticker']['base']
+    if coin_name == 'ANUAR':
         return 29
 
     return 42
